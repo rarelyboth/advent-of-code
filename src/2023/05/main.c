d00c8d2e66459e65a344bcfff9a97981018343d5
Robert Hartley <rarelyboth@gmail.com>
2023-12-06 21:40:02 +1000

    Day 5 refactor with Nic's giga chad solution

--- src/2023/05/main.c
+++ src/2023/05/main.c
@@ -4,33 +4,125 @@
 #include <string.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#include <stdbool.h>
+#include <assert.h>
 
 #define DIGITS "0123456789"
 
 typedef struct {
-    int64_t first;
-    int64_t second;
-} rPair;
+    int64_t size;
+    int64_t elements;
+    int64_t *set;
+} rSet;
 
-rPair *rPairNew(int64_t first, int64_t second) {
-    rPair *pair = calloc(1, sizeof(rPair));
+rSet *rSetNew(int64_t sizeHint) {
+    rSet *set = malloc(sizeof(rSet));
 
-    pair->first = first;
-    pair->second = second;
+    set->size = sizeHint;
+    set->elements = 0;
+    set->set = calloc(sizeHint, sizeof(int64_t));
 
-    return pair;
+    return set;
 }
 
-void rPairFree(rPair *pair) {
-    free(pair);
+void rSetAdd(rSet *set, int64_t element) {
+    if (set->elements == set->size) {
+        set->size *= 2;
+        set->set = realloc(set->set, set->size * sizeof(int64_t));
+        assert(set->set != NULL);
+    }
+
+    bool inSet = false;
+    for (int i = 0; i < set->elements; ++i) {
+        if (*(set->set + i) == element) {
+            inSet = true;
+            break;
+        }
+    }
+
+    if (!inSet) {
+        *(set->set + set->elements) = element;
+        ++(set->elements);
+    }
+}
+
+bool rSetIncludes(rSet *set, int64_t element) {
+    for (int i = 0; i < set->elements; ++i) {
+        if (*(set->set) == element) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 typedef struct {
-    rPair *domain;
-    rPair *codomain;
+    int64_t start;
+    int64_t end;
+} rRange;
+
+rRange *rRangeNew(int64_t first, int64_t second) {
+    rRange *range = calloc(1, sizeof(rRange));
+
+    range->start = first;
+    range->end = second;
+
+    return range;
+}
+
+bool rRangeContains(rRange * range, int64_t element) {
+    return range->start <= element && element < range->end;
+}
+
+void rRangeFree(rRange *range) {
+    free(range);
+}
+
+typedef struct {
+    int64_t size;
+    int64_t elements;
+    rRange **set;
+} rRangeSet;
+
+rRangeSet *rRangeSetNew(int64_t sizeHint) {
+    rRangeSet *set = malloc(sizeof(rRangeSet));
+
+    set->size = sizeHint;
+    set->elements = 0;
+    set->set = calloc(sizeHint, sizeof(rRange));
+
+    return set;
+}
+
+bool rRangeSetIncludes(rRangeSet *set, rRange * element) {
+    for (int i = 0; i < set->elements; ++i) {
+        if (*(set->set) == element) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void rRangeSetAdd(rRangeSet *set, rRange * element) {
+    if (set->elements == set->size) {
+        set->size *= 2;
+        set->set = realloc(set->set, set->size * sizeof(rRange));
+        assert(set->set != NULL);
+    }
+
+    if (!rRangeSetIncludes(set, element)) {
+        *(set->set + set->elements) = element;
+        ++(set->elements);
+    }
+}
+
+typedef struct {
+    rRange *domain;
+    rRange *codomain;
 } rFunction;
 
-rFunction *rFunctionNew(rPair *domain, rPair *codomain) {
+rFunction *rFunctionNew(rRange *domain, rRange *codomain) {
     rFunction *function = calloc(1, sizeof(rFunction));
 
     function->domain = domain;
@@ -40,23 +132,23 @@ rFunction *rFunctionNew(rPair *domain, rPair *codomain) {
 }
 
 rFunction *rFunctionNewFromRange(int64_t destination, int64_t source, int64_t range) {
-    rPair * domain = rPairNew(source, source + range);
-    rPair * codomain = rPairNew(destination, destination + range);
+    rRange * domain = rRangeNew(source, source + range);
+    rRange * codomain = rRangeNew(destination, destination + range);
 
     return rFunctionNew(domain, codomain);
 }
 
 int64_t rFunctionApply(rFunction *function, int64_t input) {
-    return input + (function->codomain - function->domain);
+    return input + (function->codomain->start - function->domain->start);
 }
 
 int64_t rFunctionUnapply(rFunction *function, int64_t input) {
-    return input - (function->codomain - function->domain);
+    return input - (function->codomain->start - function->domain->start);
 }
 
 void rFunctionFree(rFunction *function) {
-    rPairFree(function->domain);
-    rPairFree(function->codomain);
+    rRangeFree(function->domain);
+    rRangeFree(function->codomain);
     free(function);
 }
 
@@ -90,6 +182,30 @@ void rMapAdd(rMap *map, rFunction *function) {
     ++map->n;
 }
 
+int64_t rMapApplyFunction(rMap *map, int64_t input) {
+    for (int i = 0; i < map->n; ++i) {
+        rFunction * function = *(map->data + i);
+
+        if (rRangeContains(function->domain, input)) {
+            return rFunctionApply(function, input);
+        }
+    }
+
+    return input;
+}
+
+int64_t rMapUnapplyFunction(rMap *map, int64_t input) {
+    for (int i = 0; i < map->n; ++i) {
+        rFunction * function = *(map->data + i);
+
+        if (rRangeContains(function->codomain, input)) {
+            return rFunctionUnapply(function, input);
+        }
+    }
+
+    return input;
+}
+
 void rMapFree(rMap *map) {
     for (int i = 0; i < map->n; ++i) {
         rFunctionFree(*(map->data + i));
@@ -105,15 +221,6 @@ int main() {
     printf("Part Two: %" PRIi64 "\n", partTwo());
 }
 
-void rStringParseNumbers(char * const line, int64_t * const into) {
-    char * it = strpbrk(line, DIGITS);
-    for (int i = 0; it != NULL; ++i) {
-        *(into + i) = strtol(it, &it, 10);
-
-        it = strpbrk(it, DIGITS);
-    }
-}
-
 int64_t partOne() {
     FILE *file = rFileOpen("input.txt");
 
@@ -125,7 +232,7 @@ int64_t partOne() {
     int64_t * const seeds = calloc(numSeeds, sizeof(int64_t));
     rMap ** const maps = calloc(numMaps, sizeof(rMap));
 
-    // Extract seeds, which we assume is on the first line
+    // Extract seeds, which we assume is on the start line
     {
         char * seedLine = fgets(buffer, bufferSize, file);
 
@@ -137,7 +244,7 @@ int64_t partOne() {
     {
         for (int i = -1; fgets(buffer, bufferSize, file) != NULL; ) {
             char * const line = rStringCopyUntil(buffer, '\n');
-            if (strlen(line) == 0) {
+            if (*line == '\n') {
                 continue;
             } else if (strchr(line, ':') != NULL) {
                 ++i;
@@ -156,8 +263,143 @@ int64_t partOne() {
 
     fclose(file);
 
-    //
+    // Calculate the min location of all seeds to check
+    int64_t min = INT64_MAX;
     {
+        for (int i = 0; i < numSeeds; ++i) {
+            int64_t seed = *(seeds + i);
 
+            int64_t output = seed;
+            for (int j = 0; j < numMaps; ++j) {
+                rMap * map = *(maps + j);
+
+                output = rMapApplyFunction(map, output);
+            }
+
+            if (output < min) {
+                min = output;
+            }
+        }
     }
+
+    return min;
+}
+
+void rStringParseSeeds(char const * const line, rRangeSet * into) {
+    char * it = strpbrk(line, "0123456789");
+    while (it != NULL) {
+
+        int64_t first = strtol(it, &it, 10);
+        it = strpbrk(it, "0123456789");
+        int64_t second = strtol(it, &it, 10);
+
+        rRange * range = rRangeNew(first, first + second);
+
+        rRangeSetAdd(into, range);
+
+        it = strpbrk(it, "0123456789");
+    }
+}
+
+int64_t partTwo() {
+    FILE *file = rFileOpen("input.txt");
+
+    uint8_t const numSeeds = 20;
+    uint8_t const bufferSize = 255;
+    uint8_t const numMaps = 7;
+
+    char buffer[bufferSize];
+    rRangeSet * seeds = rRangeSetNew(numSeeds);
+    rMap ** const maps = calloc(numMaps, sizeof(rMap));
+
+    // Extract seed ranges, which we assume is on the start line
+    {
+        char * seedLine = fgets(buffer, bufferSize, file);
+
+        char * const line = rStringCopyUntil(buffer, '\n');
+        rStringParseSeeds(line, seeds);
+    }
+
+    // Now parse the maps
+    {
+        for (int i = -1; fgets(buffer, bufferSize, file) != NULL; ) {
+            char * const line = rStringCopyUntil(buffer, '\n');
+            if (*line == '\n') {
+                continue;
+            } else if (strchr(line, ':') != NULL) {
+                ++i;
+                *(maps + i) = rMapNew(64);
+                continue;
+            }
+
+            int64_t * const nums = calloc(3, sizeof(int64_t));
+            rStringParseNumbers(line, nums);
+
+            rFunction * function = rFunctionNewFromRange(nums[0], nums[1], nums[2]);
+
+            rMapAdd(*(maps + i), function);
+        }
+    }
+
+    fclose(file);
+
+    // Calculate the optimal set of seeds to check
+    rSet * potentialSeeds = rSetNew(64);
+    {
+        for (int i = numMaps - 1; i > 0; --i) {
+            rMap * map = *(maps + i);
+
+            for (int f = 0; f < map->n; ++f) {
+                rFunction * function = *(map->data + f);
+
+                int64_t domainStart = function->domain->start;
+                int64_t domainEnd = function->domain->end;
+                for (int j = i - 1; j >= 0; --j) {
+                    rMap * prevMap = *(maps + j);
+
+                    domainStart = rMapUnapplyFunction(prevMap, domainStart);
+                    domainEnd = rMapUnapplyFunction(prevMap, domainEnd);
+                }
+
+                rSetAdd(potentialSeeds, domainStart);
+                rSetAdd(potentialSeeds, domainEnd);
+            }
+        }
+    }
+
+    // Add the seed ranges to the list of potential seeds
+    {
+        for (int i = 0; i < seeds->elements; ++i) {
+            rRange * seedRange = *(seeds->set + i);
+            rSetAdd(potentialSeeds, seedRange->start);
+            rSetAdd(potentialSeeds, seedRange->end);
+        }
+    }
+
+    // Calculate the min location of all seeds to check
+    int64_t min = INT64_MAX;
+    {
+        for (int i = 0; i < potentialSeeds->elements; ++i) {
+            int64_t potentialSeed = *(potentialSeeds->set + i);
+
+            for (int j = 0; j < seeds->elements; ++j) {
+                rRange * seedRange = *(seeds->set + j);
+
+                if (rRangeContains(seedRange, potentialSeed)) {
+                    int64_t output = potentialSeed;
+                    for (int k = 0; k < numMaps; ++k) {
+                        rMap * map = *(maps + k);
+
+                        output = rMapApplyFunction(map, output);
+                    }
+
+                    if (output < min) {
+                        min = output;
+                    }
+                }
+            }
+        }
+    }
+
+    return min;
 }
